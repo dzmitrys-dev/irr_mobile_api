7e38283d32918e1d471a0728eb82d72e57ea2aac
Stormcore <shell32@tut.by>
2013-01-10 18:48:04 +0300

    необходимые поля

--- features/step_definitions/adverts_api_steps.rb
+++ features/step_definitions/adverts_api_steps.rb
@@ -11,6 +11,12 @@ def setfields(page_params)
         @region = hash['value']
       when "Тип объявления"
         @advert_type = hash['value']
+      when "E-mail"
+        @mail = hash['value']
+      when "Телефон"
+        @phone = hash['value']
+      when "Контактное лицо"
+        @contact = hash['value']
       when "Модель"
         @model = hash['value']
         customfieldsarray << "model" 
@@ -50,10 +56,26 @@ end
 То %{я подаю объявление со следующими параметрами:} do |page_params|
   setfields(page_params)
   response = HTTParty.post(API_URL + 'advertisements/advert',
-   :body => {:auth_token => $token, :category => @category,
-     :region => @region, :advert_type => @advert_type,
-      :advertisement => {:custom_fields => @customfields}})
+   :body => {:auth_token => $token, :category => @category, :region => @region, :advert_type => @advert_type,
+     :advertisement => {:mail => @mail, :phone => @phone, :contact => @contact, :custom_fields => @customfields}})
+  @response = JSON.parse(response)
+  puts @response
+  checkforerrors(@response)
+end
+
+То %{я запоминаю идентификатор объявления} do
+  @advertisement_id = response['id']
+  puts @advertisement_id
+end
+
+То %{я проверяю, что в объявлении содержатся следующие данные:} do |page_params|
+  response = HTTParty.get(API_URL + 'advertisements/advert/' + @advertisement_id)
   response = JSON.parse(response)
   puts response
-  checkforerrors(response)
+  checkforerrors(@response)
+  page_params.hashes.each do |hash|
+    unless response.has_value?(hash['value']) && response['advertisement'].has_value?(hash['value']) && response['advertisement']['custom_fields'].has_value?(hash['value'])
+      raise "В объявлении не найдено значение" + hash['value']
+    end
+  end
 end
