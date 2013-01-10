3492d10e3cc5504c330725b4c4aa771c4ed25e52
Stormcore <shell32@tut.by>
2013-01-10 16:33:57 +0300

    Рефакторинг установки значений, успешный тест добавления объявления

--- features/step_definitions/adverts_api_steps.rb
+++ features/step_definitions/adverts_api_steps.rb
@@ -1,7 +1,6 @@
 # coding: utf-8
 
-#TODO: создать хэш кастом-филдов и наполнять его в зависимости, какие указаны в объявлении. Сделать словарь значений полей.
-То %{я подаю объявление со следующими параметрами:} do |page_params|
+def setfields(page_params)
   customfieldsarray = []
   page_params.hashes.each do |hash|
       case hash['parameter']
@@ -9,7 +8,6 @@
         @category = hash['value']
       when "Регион"
         @region = hash['value']
-        puts @region
       when "Тип объявления"
         @advert_type = hash['value']
       when "Модель"
@@ -19,6 +17,10 @@
       when "Цена"
         @price = hash['value']
         customfieldsarray << "price" 
+        customfieldsarray << hash['value']
+      when "Валюта"
+        @price = hash['value']
+        customfieldsarray << "currency" 
         customfieldsarray << hash['value']
       when "Год выпуска"
         @year = hash['value']
@@ -33,18 +35,21 @@
         customfieldsarray << hash['value']
         @body_type = hash['value']
       when "Тип трансмиссии"
-        customfieldsarray << "transmission"
+        customfieldsarray << "transmittion"
         customfieldsarray << hash['value']
         @transmission = hash['value']
       end
   end
-  customfields = Hash[*customfieldsarray]
-  #puts customfields
-  #TODO: Передавать кастом-филды не через пару name, value, a по типу :transmission => "вариатор"
+  @customfields = Hash[*customfieldsarray]
+  puts @customfields
+end
+
+#TODO: Сделать словарь значений полей.
+То %{я подаю объявление со следующими параметрами:} do |page_params|
+  setfields(page_params)
   response = HTTParty.post(API_URL + 'advertisements/advert', :body => {:auth_token => $token, :category => "classified/cars/passenger/used/",
      :region => @region, :advert_type => "auto_sprobegom",
-      :advertisement => {:custom_fields => {:name => "model", :value => @model, :name => "price", :value => @price, :name => "car-year",
-         :value => @year, :name => "make", :value => @mark, :name => "bodytype", :value => @body_type, :name => "transmission", :value => @transmission}}})
+      :advertisement => {:custom_fields => @customfields}})
   response = JSON.parse(response)
   puts response
   checkforerrors(response)
