ee7c2a942e60409ab29fe457b3cb104f489cc1a0
stormy <stormy@stormy-virtual-machine.(none)>
2013-01-14 18:30:35 +0300

    Сценарий: Поиск объявления по критериям

--- features/step_definitions/adverts/adverts_api_steps.rb
+++ features/step_definitions/adverts/adverts_api_steps.rb
@@ -58,6 +58,8 @@ def setfields(page_params)
         @include_privates = hash['value']
       when "Включить коммерческие"
         @include_compannies = hash['value']
+      when "Ключевые слова"
+        @keywords = hash['value']
       end
   end
   @customfields = Hash[*customfieldsarray]
@@ -198,6 +200,20 @@ end
   setfields(page_params)
   response = HTTParty.get(API_URL + 'advertisements/popular',
    :body => {:category => @category, :region => @region, :currency => @currency})
+  @response = JSON.parse(response)
+  puts @response
+  checkforerrors(@response)
+  if @response['advertisements'] == nil
+    raise "Невозможно получить объявления по заданным критериям"
+  end
+end
+
+То %{я осуществляю поиск объявлений по следующим критериям:} do |page_params|
+  setfields(page_params)
+  response = HTTParty.get(API_URL + 'advertisements/search',
+   :body => {:category => @category, :region => @region, :currency => @currency,
+     :offset => @offset, :limit => @limit, :sort_by => @sort_by, :keywords => @keywords,
+      :custom_fields => @customfields})
   @response = JSON.parse(response)
   puts @response
   checkforerrors(@response)
