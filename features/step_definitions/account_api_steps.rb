30f8600e2b09c1aed1dbd72ec39c8d0b4a69dc75
Stormcore <shell32@tut.by>
2013-01-19 15:31:04 +0300

    Ожидаемый результат изменения профиля

--- features/step_definitions/account_api_steps.rb
+++ features/step_definitions/account_api_steps.rb
@@ -13,11 +13,17 @@ end
   #TODO: Проверить, можно ли заполнять критичные поля типа login
   phone = 1000000 + Random.rand(999999)
   mobile = 1000000 + Random.rand(999999)
-  phone, mobile = "+" + phone.to_s,"+" +  mobile.to_s
-  puts "Телефон: " + phone
-  puts "Мобильный: " + mobile
-  response = HTTParty.put(API_URL + 'account', :body => {:auth_token => $token, :user_info =>{:phone => phone, :mobile => mobile}})
-  response = JSON.parse(response)
-  puts response
-  checkforerrors(response)
+  @phone, @mobile = "+" + phone.to_s,"+" +  mobile.to_s
+  puts "Телефон: " + @phone
+  puts "Мобильный: " + @mobile
+  response = HTTParty.put(API_URL + 'account', :body => {:auth_token => $token, :user_info =>{:phone => @phone, :mobile => @mobile}})
+  @response = JSON.parse(response)
+  puts @response
+  checkforerrors(@response)
+end
+
+То %{в профиле сохранены введенные данные} do
+  unless @response['user_info']['phone'] == @phone && @response['user_info']['mobile'] == @mobile
+    raise "Данные в профиле не соответствуют заполненным: " + @response.to_s
+  end
 end
